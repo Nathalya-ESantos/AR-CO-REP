88571749469831e153799edc8c97bb2529240f09
nathalya santos <nathalya.e.santos@aluno.senai.br>
2026-02-27 08:33:52 -0300

    fazendo os botões

--- js/main.js
+++ js/main.js
@@ -1,28 +1,33 @@
-// 1. Lógica do menu principal
-
+// 1. lógica do menu principal
 const btnP1 = document.querySelector('#inputP1');
 const btnP2 = document.querySelector('#inputP2');
 const btnP3 = document.querySelector('#inputP3');
 
-// 2. Mdetódo addEventListener com uma função anonima
-
-btnP1.addEventListener('click', (){
-    window.location.href='./pages/projeto01.html'
+// 2. metódo addEventListener com uma função anônima
+btnP1.addEventListener('click', function () {
+    window.location.href = './pages/projeto01.html';
 });
 
-btnP2.addEventListener('click', function(){
-    window.location.href='./pages/projeto02.html'
+btnP2.addEventListener('click', function () {
+    window.location.href = './pages/projeto02.html';
 });
 
-btnP3.addEventListener('click', function(){
-    window.location.href='./pages/projeto03.html'
+btnP3.addEventListener('click', function () {
+    window.location.href = './pages/projeto03.html';
 });
 
-function cacho(x,y){
-    return x + y
-};
 
-const banana = cacho
+//     function cacho(x, y) {
+//     return x + y
 
-//Arrow Function
-const uva = () => 'Sou uma fruta';
+//     const banana = cacho
+//     // arrow function
+//     const uva =  () => 'sou uma fruta';
+
+//     // exemplo funcao literal
+//     function uva() {
+//         return('sou uma fruta!!!!')
+//     }
+
+
+// };
